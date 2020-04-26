cf2c77fa18b0f1aeca552f97041b19228a5fe2f1
n.miyaho@gmail.com <n.miyaho@gmail.com>
2020-04-26 13:50:20 +0900

    データベース部分をcommon.phpへ移行

--- php/www/html/list.php
+++ php/www/html/list.php
@@ -8,15 +8,7 @@
 <body>
 
 <?php
-// 　　データと接続
-$dsn = 'mysql:dbname=cookie_site;host=db';
-$user = 'root';
-$password = 'root_password';
-$dbh = new PDO($dsn,$user,$password);
-$dbh->query('SET NAMES utf8');
-// 　　　SQLの指令文
-$stmt = $dbh->prepare("SELECT*FROM members");
-$stmt->execute();
+require("common.php");
 ?>
 
 <table><tbody>
